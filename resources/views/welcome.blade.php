a9690ca09b51c60d67ce7004e9634df872fb0837
Gerhard Theunissen <gerhard@ictglobe.com>
2020-03-06 12:12:02 +0200

    Updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Dummy ;)
                 </div>
 
                 <div class="links">
