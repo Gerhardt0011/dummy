ab37c9dee8545c393f47369a41a621a7342a29c3
Gerhard Theunissen <gerhard@ictglobe.com>
2020-03-06 12:18:56 +0200

    Dummy v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Dummy ;)
+                    Dummy V2 ;)
                 </div>
 
                 <div class="links">
